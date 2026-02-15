47bb3b01e2c7eab79dc48378dc365c38f0166abd
mileszh <mileszh05@gmail.com>
2026-02-14 20:14:41 -0500

    todos-08: Implement todo filtering

--- src/main.js
+++ src/main.js
@@ -16,8 +16,20 @@ function renderTodos() {
   const todoListElement = document.getElementById("todo-list");
   todoListElement.innerHTML = "";
 
+  const filteredTodos = [];
   for (let i = 0; i < todos.length; i++) {
     const todo = todos[i];
+    if (filter === "all") {
+      filteredTodos.push(todo);
+    } else if (filter === "completed" && todo.completed) {
+      filteredTodos.push(todo);
+    } else if (filter === "active" && todo.completed == false) {
+      filteredTodos.push(todo);
+    }
+  }
+
+  for (let i = 0; i < filteredTodos.length; i++) {
+    const todo = filteredTodos[i];
 
     const todoItem = document.createElement("div");
     todoItem.classList.add("p-4", "todo-item");
@@ -38,6 +50,32 @@ function renderTodos() {
   }
 }
 
+function renderTodoNavBar(hrefValue) {
+  debugger;
+  const todoNav = document.getElementById("todo-nav");
+  const elements = todoNav.children;
+
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+
+    if (element.href === hrefValue) {
+      element.classList.add(
+        "underline",
+        "underline-offset-4",
+        "decoration-rose-700",
+        "decoration-2",
+      );
+    } else {
+      element.classList.remove(
+        "underline",
+        "underline-offset-4",
+        "decoration-rose-700",
+        "decoration-2",
+      );
+    }
+  }
+}
+
 function handleNewTodoKeyDown(event) {
   const newTodoInput = event.target;
   const todoText = newTodoInput.value.trim();
@@ -53,7 +91,23 @@ function handleNewTodoKeyDown(event) {
   }
 }
 
+function handleClickOnNavbar(event) {
+  if (event.target.tagName === "A") {
+    // "#/completed"
+    // ["#", "completed"]
+    // "completed"
+    const hrefValue = event.target.href;
+    const action = hrefValue.split("/").pop();
+    filter = action === "" ? "all" : action;
+    renderTodos();
+    renderTodoNavBar(hrefValue);
+  }
+}
+
 const newTodoInput = document.getElementById("new-todo");
 newTodoInput.addEventListener("keydown", handleNewTodoKeyDown);
+
+const todoNav = document.getElementById("todo-nav");
+todoNav.addEventListener("click", handleClickOnNavbar);
 
 document.addEventListener("DOMContentLoaded", renderTodos());
