a97ce238da0e03c17e23bfa7925d5699627c9b44
mileszh <mileszh05@gmail.com>
2026-02-15 18:12:53 -0500

    todos-17: Add static members and export Todo class

--- src/todo.js
+++ src/todo.js
@@ -1,19 +1,38 @@
 class Todo {
+  static #nextId = 1;
   #id;
+  #text;
+  #completed;
 
-  constructor(id, text, completed = false) {
-    this.#id = id;
-    this.text = text;
-    this.completed = completed;
+  constructor(text, completed = false) {
+    this.#id = Todo.#nextId++;
+    this.#text = text;
+    this.#completed = completed;
   }
 
   toggle() {
-    this.completed = !this.completed;
+    this.#completed = !this.#completed;
   }
 
-  get Id() {
+  get id() {
     return this.#id;
+  }
+
+  get text() {
+    return this.#text;
+  }
+
+  get completed() {
+    return this.#completed;
+  }
+
+  set text(newText) {
+    this.#text = newText;
+  }
+
+  set completed(value) {
+    this.#completed = value;
   }
 }
 
-const todo = new Todo(1, "Buy milk");
+export default Todo;
