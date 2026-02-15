0f7cab8bad6fdb4fc1f6fabb46444c039db84d1a
mileszh <mileszh05@gmail.com>
2026-02-14 19:53:42 -0500

    todos-07: Initialize state and render todos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,59 @@
 import "./style.css";
 
-const app = document.getElementById("app");
-app.innerHTML = `<div class="flex items-center justify-center h-screen"> 
-  <div>Hello World!<div/>
-</div>`;
+// user interaction -> state changes -> UI change
+
+// State of the app
+const todos = [
+  { id: 1, text: "Buy coffee", completed: false },
+  { id: 2, text: "Buy bread", completed: false },
+  { id: 3, text: "Buy jam", completed: true },
+];
+
+let nextTodoId = 4;
+let filter = "all"; // can be "all", "active", "completed"
+
+function renderTodos() {
+  const todoListElement = document.getElementById("todo-list");
+  todoListElement.innerHTML = "";
+
+  for (let i = 0; i < todos.length; i++) {
+    const todo = todos[i];
+
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("p-4", "todo-item");
+    todoListElement.appendChild(todoItem);
+
+    const todoText = document.createElement("div");
+    todoText.classList.add("todo-text");
+    if (todo.completed) {
+      todoText.classList.add("line-through");
+    }
+    todoText.textContent = todo.text;
+    todoItem.appendChild(todoText);
+
+    const todoEdit = document.createElement("div");
+    todoEdit.classList.add("hidden", "todo-edit");
+    todoEdit.value = todo.text;
+    todoItem.appendChild(todoEdit);
+  }
+}
+
+function handleNewTodoKeyDown(event) {
+  const newTodoInput = event.target;
+  const todoText = newTodoInput.value.trim();
+
+  if (event.key === "Enter" && todoText !== "") {
+    todos.push({
+      id: nextTodoId++,
+      text: todoText,
+      completed: false,
+    });
+    newTodoInput.value = "";
+    renderTodos();
+  }
+}
+
+const newTodoInput = document.getElementById("new-todo");
+newTodoInput.addEventListener("keydown", handleNewTodoKeyDown);
+
+document.addEventListener("DOMContentLoaded", renderTodos());
